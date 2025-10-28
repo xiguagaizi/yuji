1ddf1a8ebad5e81c3698d64331670a021a2a70f5
xiguagaizi <1598391039@qq.com>
2025-10-28 16:52:56 +0800

    new readme

--- run_app.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set ANDROID_HOME=C:\Users\gmc\AppData\Local\Android\Sdk
-set PATH=%ANDROID_HOME%\platform-tools;%ANDROID_HOME%\tools;%PATH%
-cd /d d:\my_little_apk\yuji
-flutter run -d AJTLVB4B15003585 --no-fast-start
-
-
